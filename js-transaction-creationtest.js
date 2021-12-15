1e75d014b2a6cf1c4caff24e0d480100843ede72
PurpleMongrel <thaispdf@gmail.com>
2021-12-15 01:39:55 -0500

    console.log for testing

--- js-transaction-creationtest.js
+++ js-transaction-creationtest.js
@@ -5,6 +5,7 @@ const txData = {
   nonce: '0x0',
   gasPrice: '0x09184e72a000',
   gasLimit: '0x30000',
+  dummy: '0x',
   to: '0xb0920c523d582040f2bcb1bd7fb1c7c1ecebdb34',
   value: '0x00',
   data: "0x",
@@ -14,8 +15,15 @@ const txData = {
 };
 
 tx = new ethTx(txData);
-console.log(`tx: ${tx.toString()}`)
-
-for(var property in tx) {
-  console.log(property + "=" + tx[property]);
-}
+console.log(`\ntx.serialize():\n`);
+console.log(tx.serialize());
+console.log(`\n\n\ntx.serialize().toString('hex'):\n`);
+console.log(tx.serialize().toString('hex'));
+console.log(`\n\n\ntx:\n`);
+console.log(tx);
+console.log(`\n\n\n`);
+console.log(`tx.toJSON():`);
+console.log(tx.toJSON())
+console.log(`\n\n\n`);
+console.log(`tx.validateSignature():`);
+console.log(tx.getSenderAddress())
